f1663ef2dcbf716f605956bdbd875c1b0f7f1be0
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-06-15 03:22:48 -0300

    loading

--- ES6/api.js
+++
@@ -1,3 +0,0 @@
-const { default: Axios } = require("axios");
-
-import axios from 'axios'
--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,30 +1,63 @@
+import api from './api'
+
 class App{
     constructor(){
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.inputEL = document.querySelector('input[name=repository]')
         this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     };
 
     registerHandlers(){
-        this.formEl.onsubmit = event => this.addRepository(event)
+        this.formEl.onsubmit = event => this.addRepository(event);
+    };
 
+    setLoading(loading = true){
+        if(loading===true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading')
+
+            this.formEl.appendChild(loadingEl)
+        }else{
+            document.getElementById('loading').remove();
+        }
     }
 
-    addRepository(){
+    async addRepository(){
         event.preventDefault();
 
-        this.repositories.push({
-            name: 'rocketseat.com.br',
-            description: 'Tire a sua ideia do papel e dê vida a sua startup',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'http://github.com/rocketseat/react-native-template-rocketseat-basic'
-        });
+        const repoInput = this.inputEL.value;
 
-        this.render();
+        if (repoInput.length === 0)
+            return;
 
-        console.log(this.repositories);
+        this.setLoading();
+        
+        try{
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: {avatar_url}} = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEL.value = '';
+
+            this.render();
+        }catch(err){
+            alert('O repositorio não existe!')
+        }
+
+        this.setLoading(false);
+
     };
+
 
     render(){
         this.listEl.innerHTML = '';
@@ -41,6 +74,7 @@ class App{
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blanck');
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
